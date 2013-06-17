e09953dafcb0d66b1365cbfd8e7c3cf7858fffb4
thunder <thunderace@free.fr>
2013-06-17 11:04:32 +0200

    Update for openWRT

--- noipupdater.sh
+++ noipupdater.sh
@@ -4,20 +4,19 @@
 USERNAME=username
 PASSWORD=password
 HOST=hostsite
-LOGFILE=logdir/noip.log
-STOREDIPFILE=configdir/current_ip
-USERAGENT="Simple Bash No-IP Updater/0.4 antoniocs@gmail.com"
+LOGFILE=./noip.log
+STOREDIPFILE=./current_ip
+USERAGENT="Simple Bash No-IP Updater/0.4 for openWRT"
 
 if [ ! -e $STOREDIPFILE ]; then 
 	touch $STOREDIPFILE
 fi
 
-NEWIP=$(wget -O - http://icanhazip.com/ -o /dev/null)
+NEWIP=$(wget -q -O - http://icanhazip.com/) 
 STOREDIP=$(cat $STOREDIPFILE)
 
 if [ "$NEWIP" != "$STOREDIP" ]; then
-	RESULT=$(wget -O "$LOGFILE" -q --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
-
+	RESULT=$(wget -O "$LOGFILE" -q --user-agent="$USERAGENT" "http://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP") 
 	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
 	echo $NEWIP > $STOREDIPFILE
 else
